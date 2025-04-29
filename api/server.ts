3f3d9a68e552712b9a9964a9cd48ff14d3d076e7
kazuya-shimatani <kzy.shimatani@gmail.com>
2025-04-29 19:18:59 +0900

    feat: getTotalMonthlyDuration実装

--- api/server.ts
+++ api/server.ts
@@ -1,18 +1,40 @@
 import { z } from "zod";
 import { initializeMcpApiHandler } from "../lib/mcp-api-handler";
+import { TogglTrackClient } from "../lib/toggle-track";
+
+const TOGGL_TRACK_API_TOKEN = process.env.TOGGL_TRACK_API_TOKEN;
+
+if (!TOGGL_TRACK_API_TOKEN) {
+  throw new Error("TOGGL_TRACK_API_TOKEN is not set");
+}
+
+const togglTrackClient = new TogglTrackClient(TOGGL_TRACK_API_TOKEN);
 
 const handler = initializeMcpApiHandler(
   (server) => {
     // Add more tools, resources, and prompts here
-    server.tool("echo", { message: z.string() }, async ({ message }) => ({
-      content: [{ type: "text", text: `Tool echo: ${message}` }],
-    }));
+    // server.tool("echo", { message: z.string() }, async ({ message }) => ({
+    //   content: [{ type: "text", text: `Tool echo: ${message}` }],
+    // }));
+
+    server.tool("getTimeEntries", { 
+      startDate: z.string(),  // 日付型のパラメータ
+      endDate: z.string()   // 日付型のパラメータ
+    }, async ({ startDate, endDate }) => {
+      const data = await togglTrackClient.getTotalMonthlyDuration(startDate, endDate);
+      return {
+        content: [{ type: "text", text: `Result: ${JSON.stringify(data)}` }],
+      };
+    });
   },
   {
     capabilities: {
       tools: {
-        echo: {
-          description: "Echo a message",
+        // echo: {
+        //   description: "Echo a message",
+        // },
+        getTimeEntries: {
+          description: "Get span time entries",
         },
       },
     },
---
+++ lib/toggle-track.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const TOGGL_TRACK_API_BASE_URL = 'https://api.track.toggl.com/api/v9';
+
+const toUTCISOString = (date: Date) => (new Date(date.getTime() - (9 * 60 * 60 * 1000))).toISOString();
+
+export class TogglTrackClient {
+  private apiToken: string;
+
+  constructor(apiToken: string) {
+    this.apiToken = apiToken;
+  }
+
+  private getAuthHeader() {
+    return {
+      'Content-Type': "application/json",
+      Authorization: `Basic ${Buffer.from(`${this.apiToken}:api_token`).toString('base64')}`,
+    };
+  }
+
+  async getTotalMonthlyDuration(inputStartDate?: string, inputEndDate?: string) {
+    const now = new Date();
+    const startDate = toUTCISOString(inputStartDate ? new Date(inputStartDate) : new Date(now.getFullYear(), now.getMonth(), 0, 23, 59, 59));
+    const endDate = toUTCISOString(inputEndDate ? new Date(inputEndDate) : new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59));
+
+    const response = await axios.get(`${TOGGL_TRACK_API_BASE_URL}/me/time_entries`, {
+        headers: this.getAuthHeader(),
+        params: {
+          start_date: startDate,
+          end_date: endDate,
+        },
+      });
+
+    return response.data;
+  }
+} 
