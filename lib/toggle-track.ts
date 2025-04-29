4d7c9eb4c2f37aa2432dc0ded0f1ab2a62241965
kazuya-shimatani <kzy.shimatani@gmail.com>
2025-04-29 21:24:26 +0900

    fix: 月末判定のロジックを見直し

--- lib/toggle-track.ts
+++ lib/toggle-track.ts
@@ -21,7 +21,7 @@ export class TogglTrackClient {
   async getTotalMonthlyDuration(inputStartDate?: string, inputEndDate?: string) {
     const now = new Date();
     const startDate = toUTCISOString(inputStartDate ? new Date(inputStartDate) : new Date(now.getFullYear(), now.getMonth(), 0, 23, 59, 59));
-    const endDate = toUTCISOString(inputEndDate ? new Date(inputEndDate) : new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59));
+    const endDate = toUTCISOString(inputEndDate ? new Date(new Date(inputEndDate).setHours(23, 59, 59, 999)) : new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59, 999));
 
     const response = await axios.get(`${TOGGL_TRACK_API_BASE_URL}/me/time_entries`, {
         headers: this.getAuthHeader(),
