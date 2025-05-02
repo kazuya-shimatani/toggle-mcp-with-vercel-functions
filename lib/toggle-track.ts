daf627d0038b8dc383690be485fecfdb574b481a
kazuya-shimatani <kzy.shimatani@gmail.com>
2025-05-02 22:57:54 +0900

    feat: console.log追加

--- lib/toggle-track.ts
+++ lib/toggle-track.ts
@@ -23,6 +23,9 @@ export class TogglTrackClient {
     const startDate = toUTCISOString(inputStartDate ? new Date(inputStartDate) : new Date(now.getFullYear(), now.getMonth(), 0, 23, 59, 59));
     const endDate = toUTCISOString(inputEndDate ? new Date(new Date(inputEndDate).setHours(23, 59, 59, 999)) : new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59, 999));
 
+    console.log(`startDate: ${startDate}`);
+    console.log(`endDate: ${endDate}`);
+
     const response = await axios.get(`${TOGGL_TRACK_API_BASE_URL}/me/time_entries`, {
         headers: this.getAuthHeader(),
         params: {
